d9a393ef4d0abec9621d5ae72398271060022445
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-08-09 10:44:07 -0400

    Created check to check for zero, (also checked for zero values)

--- answer.js
+++ answer.js
@@ -10,6 +10,7 @@ const AssignmentGroup = {
   name: "Fundamentals of JavaScript",
   course_id: 451,
   group_weight: 25,
+
   assignments: [
     {
       id: 1,
@@ -30,6 +31,30 @@ const AssignmentGroup = {
       points_possible: 500
     }
   ]
+/* //Check for zero
+  assignments: [
+    {
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50
+    },
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500
+    }
+  ]
+*/
+
+
 };
 
 // The provided learner submission data.
