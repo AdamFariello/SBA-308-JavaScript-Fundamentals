369355eb3da26f454d45956c9ac34a0881650d6b
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-08-09 11:05:29 -0400

    Added javascript module files to make it easier to do testing

--- answer.js
+++ answer.js
@@ -1,108 +1,4 @@
-// The provided course information.
-const CourseInfo = {
-  id: 451,
-  name: "Introduction to JavaScript"
-};
-
-// The provided assignment group.
-const AssignmentGroup = {
-  id: 12345,
-  name: "Fundamentals of JavaScript",
-  course_id: 451,
-  group_weight: 25,
-
-  assignments: [
-    {
-      id: 1,
-      name: "Declare a Variable",
-      due_at: "2023-01-25",
-      points_possible: 50
-    },
-    {
-      id: 2,
-      name: "Write a Function",
-      due_at: "2023-02-27",
-      points_possible: 150
-    },
-    {
-      id: 3,
-      name: "Code the World",
-      due_at: "3156-11-15",
-      points_possible: 500
-    }
-  ]
-/* //Check for zero
-  assignments: [
-    {
-      id: 1,
-      name: "Declare a Variable",
-      due_at: "2023-01-25",
-      points_possible: 50
-    },
-    {
-      id: 2,
-      name: "Write a Function",
-      due_at: "2023-02-27",
-      points_possible: 150
-    },
-    {
-      id: 3,
-      name: "Code the World",
-      due_at: "3156-11-15",
-      points_possible: 500
-    }
-  ]
-*/
-
-
-};
-
-// The provided learner submission data.
-const LearnerSubmissions = [
-  {
-    learner_id: 125,
-    assignment_id: 1,
-    submission: {
-      submitted_at: "2023-01-25",
-      score: 47
-    }
-  },
-  {
-    learner_id: 125,
-    assignment_id: 2,
-    submission: {
-      submitted_at: "2023-02-12",
-      score: 150
-    }
-  },
-  {
-    learner_id: 125,
-    assignment_id: 3,
-    submission: {
-      submitted_at: "2023-01-25",
-      score: 400
-    }
-  },
-  {
-    learner_id: 132,
-    assignment_id: 1,
-    submission: {
-      submitted_at: "2023-01-24",
-      score: 39
-    }
-  },
-  {
-    learner_id: 132,
-    assignment_id: 2,
-    submission: {
-      submitted_at: "2023-03-07",
-      score: 140
-    }
-  }
-];
-
-
-
+import {CourseInfo, AssignmentGroup, LearnerSubmissions} from "./import.main.mjs";
 
 //function getLearnerData(CourseInfo, AssignmentGroup, [LearnerSubmission]) 
 function getLearnerData(course, ag, submissions) {	
@@ -123,7 +19,7 @@ function getLearnerData(course, ag, submissions) {
 
 	// Get Properties functions
 	function getAssignment(learnerSubmitId) {
-		return assignment = AssignmentGroup.assignments.filter(e => {
+		return AssignmentGroup.assignments.filter(e => {
 			return e.id === learnerSubmitId  
 		})[0] 
 	}
@@ -153,6 +49,7 @@ function getLearnerData(course, ag, submissions) {
 		//TODO: Clean up function, really sloppy
 		let numerator = 0
 		let denominator = 0
+		let submission = null
 		for (submission of submissionsArray) {
 			const key = Object.keys(submission)[0]
 			const arr = submission[key]
@@ -182,6 +79,7 @@ function getLearnerData(course, ag, submissions) {
 
 	let result = {}
 	let submissionsArray = []
+	let LearnerSubmission = null
 	for (LearnerSubmission of LearnerSubmissions) {
 		if (Object.keys(result).length !== 0 && 
 			LearnerSubmission.learner_id !== result.id) {
